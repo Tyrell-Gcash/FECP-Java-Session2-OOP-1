f36940559ddacefe93e55d262508cc3b2118d372
Tyrell Teppang <tyrellmarcus.teppang@gcash.com>
2025-07-03 17:20:09 +0800

    Final Submission, Java Unit OOP1 Project

--- JavaBankAccountSystem/src/main/java/org/example/BankAccount.java
+++ JavaBankAccountSystem/src/main/java/org/example/BankAccount.java
@@ -6,6 +6,11 @@ public class BankAccount {
     private String accountName;
     private double accountBalance = 0;
 
+    public BankAccount(int accountNumber, String accountName){
+        this.accountNumber = accountNumber;
+        this.accountName = accountName;
+    }
+
     public int getAccountNumber() {
         return accountNumber;
     }
